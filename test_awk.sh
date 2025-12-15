c28fc4ac217106d2cc6c55b20d8bcca8f1da4947
Steffen Bollmann <stebo85@users.noreply.github.com>
2025-12-15 07:05:55 -0800

    cleanup

--- test_awk.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-prompt_file="Clinical_Analysis/CLIN-003-Predict-cognitive-decline-from-OASIS/prompt.md"
-echo "Testing awk on $prompt_file"
-awk '/^## Evidence Required/{flag=1; next} /^##/{flag=0} flag && NF {print; exit}' "$prompt_file"
